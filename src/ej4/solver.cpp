fff90a3c44ee3c446371a8b9cd0832a920c7a2dd
Shai <shai.bianchi@mercadolibre.com>
2016-09-29 00:42:40 -0300

    [Exercise 4 - src] input processing into adjacency list and queries vector

--- src/ej4/solver.cpp
+++ src/ej4/solver.cpp
@@ -5,4 +5,28 @@
 **  Problem solver
 */
 
-void run_solver() {}
+void run_solver() {
+    size_t a, p, q;
+    int v1, v2, f, t;
+    query qry;
+
+    cin >> a >> p;
+    vector<vector<int>> g(a); // adjacency list
+
+    for (size_t i = 0; i < p; i++) {
+        cin >> v1 >> v2;
+        g[v1-1].push_back(v2-1);
+    }
+
+    cin >> q;
+    vector<query> qs(q); // queries
+
+    for (size_t i = 0; i < q; i++) {
+        cin >> f >> t;
+        qry.origin = f-1;
+        qry.destination = t-1;
+        qs.push_back(qry);
+    }
+
+    // cout << g << endl;
+}
