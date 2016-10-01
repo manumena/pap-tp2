f89eaeeea3c87f3b9b6388a499a2dba247f2d1f6
Shai <shai.bianchi@mercadolibre.com>
2016-10-01 01:51:04 -0300

    [Exercise 4 - src] Code for running tests

--- src/ej4/test.cpp
+++ src/ej4/test.cpp
@@ -6,4 +6,47 @@
 **  Unit tests
 */
 
-void run_unit_tests() {}
+struct test_case_input {
+    string input_filename;
+    string expected_filename;
+};
+
+vector<test_case_input> cases_filenames = {
+    {"ej4/cases/ssd/ssd.dat", "ej4/cases/ssd/ssd.out"},
+    {"ej4/cases/sss/sss.dat", "ej4/cases/sss/sss.out"},
+    {"ej4/cases/vsc/vsc.dat", "ej4/cases/vsc/vsc.out"},
+    {"ej4/cases/vsdc/vsdc.dat", "ej4/cases/vsdc/vsdc.out"},
+    {"ej4/cases/vsdd/vsdd.dat", "ej4/cases/vsdd/vsdd.out"}
+};
+
+vector<bool> read_results(istream& is) {
+    size_t n;
+    string a;
+
+    is >> n;
+
+    vector<bool> result(n);
+    for (size_t i = 0; i < n; ++i) {
+        is >> a;
+        result[i] = a == "S";
+    }
+
+    return result;
+}
+
+void run_unit_tests() {
+    for (test_case_input& c : cases_filenames) {
+        auto run_case = [&c] () {
+            ifstream input_file(c.input_filename);
+            ifstream expected_file(c.expected_filename);
+
+            adj_list input_graph = read_graph(input_file);
+            vector<query> input_queries = read_queries(input_file);
+
+            vector<bool> obtained = solve(input_graph, input_queries);
+            
+            ASSERT(obtained == read_results(expected_file));
+        };
+        RUN_TEST (run_case);
+    }
+}
