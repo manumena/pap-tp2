062375fd300d687ee394d98ded417998629c61f2
Martin Jedwabny <martiniedva@gmail.com>
2016-10-05 01:19:02 -0300

    [Exercise 3] Bug Fix (Bridge components), Comment Extract Components

--- src/ej3/solver.cpp
+++ src/ej3/solver.cpp
@@ -72,23 +72,23 @@ vector<bool> articulation_point;
 vector<bool> bridge;
 vector<int> bridge_component_of_node;
 vector<int> bridge_component_nodes;
-stack<Edge> pila;
+// stack<Edge> pila;
 
-void extract_biconnected_component(int v, int w) {
-	while (pila.top() != Edge(v, w) && pila.top() != Edge(w, v)) {
-		cout << pila.top().first << "-" << pila.top().second << ", ";
-		pila.pop();
-	}
-	cout << pila.top().first << "-" << pila.top().second << endl;
-	pila.pop();
-}
+// void extract_biconnected_component(int v, int w) {
+// 	while (pila.top() != Edge(v, w) && pila.top() != Edge(w, v)) {
+// 		cout << pila.top().first << "-" << pila.top().second << ", ";
+// 		pila.pop();
+// 	}
+// 	cout << pila.top().first << "-" << pila.top().second << endl;
+// 	pila.pop();
+// }
 
 void calculate_biconnected_components(Graph& grafo, int v, int d, int parent) {
 	depth[v] = d;
 	low[v] = d;
-	if (v != parent) {
-		pila.push(Edge(parent, v));
-	}
+	// if (v != parent) {
+	// 	pila.push(Edge(parent, v));
+	// }
 	int children = 0;
 	for (list< pair<int, int> >::iterator it = grafo.adjacency[v].begin(); it != grafo.adjacency[v].end(); ++it) {
 		pair<int, int> c = *it;
@@ -99,9 +99,9 @@ void calculate_biconnected_components(Graph& grafo, int v, int d, int parent) {
 				++children;
 				calculate_biconnected_components(grafo, w, d+1, v);
 				low[v] = min(low[v], low[w]);
-				if (low[w] >= depth[v]) {
-					extract_biconnected_component(v, w);
-				}
+				// if (low[w] >= depth[v]) {
+				// 	extract_biconnected_component(v, w);
+				// }
 				if ((v != parent && low[w] >= depth[v]) || (v == parent && children > 1)) {
 					articulation_point[v] = true;
 				}
@@ -110,13 +110,15 @@ void calculate_biconnected_components(Graph& grafo, int v, int d, int parent) {
 				}
 			} else {
 				if (depth[w] < low[v]) {
-					pila.push(Edge(w, v));
+					// pila.push(Edge(w, v));
 					low[v] = depth[w];
 				}
 			}
 		}
 	}
 }
+
+int component_counter = 0;
 
 void calculate_bridge_components(Graph& grafo, int v, int component) {
 	bridge_component_of_node[v] = component;
@@ -127,7 +129,8 @@ void calculate_bridge_components(Graph& grafo, int v, int component) {
 		int w = c.second;
 		if (bridge_component_of_node[w] == -1) {
 			if (bridge[e]) {
-				calculate_bridge_components(grafo, w, component+1);
+				++component_counter;
+				calculate_bridge_components(grafo, w, component_counter);
 			} else {
 				calculate_bridge_components(grafo, w, component);
 			}
@@ -175,7 +178,7 @@ void run_solver() {
 	bridge = vector<bool>(grafo.M, false);
 	bridge_component_of_node = vector<int>(grafo.N, -1);
 	bridge_component_nodes = vector<int>(grafo.N, 0);
-	pila = stack<Edge>();
+	// pila = stack<Edge>();
 	calculate_biconnected_components(grafo, 0, 0, 0);
 	calculate_bridge_components(grafo, 0, 0);
 
@@ -185,11 +188,11 @@ void run_solver() {
 	// cout << articulation_point << endl;
 	// cout << bridge << endl;
 
-	cout << endl << "[";
-	for (int i = 0; i < N; ++i) {
-		cout << bridge_component_nodes[bridge_component_of_node[i]] << ", ";
-	}
-	cout << "]" << endl;
+	// cout << endl << "[";
+	// for (int i = 0; i < N; ++i) {
+	// 	cout << bridge_component_nodes[bridge_component_of_node[i]] << ", ";
+	// }
+	// cout << "]" << endl;
 
 	//Handle queries
 	// for (int i = 0; i < Q; ++i)
